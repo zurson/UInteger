5d225c0d7b2bdaf69664cce60f32ec6f391d36d5
zurson <matu.jurek@gmail.com>
2023-10-18 09:11:06 +0200

    Removed test code from main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,10 @@
 package org.example;
 
-import org.example.Exceptions.UIntegerException;
-
 public class Main {
 
     public static void main(String[] args) {
-        UInteger result;
 
-        Integer integer;
 
-        try {
-            UInteger x = new UInteger("5");
-            UInteger y = new UInteger("6");
-
-            int xd = UInteger.reverseBytes(x);
-            System.out.println("xd: " + xd);
-
-        } catch (UIntegerException e) {
-            System.out.println(e.getMessage());
-            return;
-        }
-
-//        System.out.println("\nResult: " + result.getValue());
 
     }
 
